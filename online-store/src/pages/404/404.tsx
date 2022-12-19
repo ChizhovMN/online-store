caf7396567afcf9aab1da53632958a8b71107e1d
Maxim Chizhov <chizhov98ip@gmail.com>
2022-12-19 16:34:53 +0300

    feat:add error page

--- online-store/src/pages/404/404.tsx
+++ online-store/src/pages/404/404.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import '../404/404.css';
+// import errorImg from '../../assets/images/error.png';
 
 function Error() {
-  return <div className="error-page">ERROR PAGE</div>;
+  return (
+    <div className="error-page">
+      <figure className="error-wrapper">
+        <img
+          className="error-image"
+          src="https://www.pngkey.com/png/detail/212-2123376_erreur-404-png-error-404.png"
+          alt="404"
+        />
+        <figcaption className="error-description text">Oops! Page not Found</figcaption>
+      </figure>
+    </div>
+  );
 }
 export { Error };
