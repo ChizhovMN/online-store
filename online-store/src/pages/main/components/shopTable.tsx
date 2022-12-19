d038532ce41216fb84ba779c2f5075690ae7b29f
Maxim Chizhov <chizhov98ip@gmail.com>
2022-12-19 17:49:49 +0300

    feat:add route to product page

--- online-store/src/pages/main/components/shopTable.tsx
+++ online-store/src/pages/main/components/shopTable.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import { Products } from '../../../types';
+import { Link, Route, Navigate } from 'react-router-dom';
 function ShopTable(props: Products[]) {
   const data = Object.values(props);
   return (
     <div className="shop-table">
       {data.map(({ thumbnail, group, album, year, format, price, id }: Products) => {
         return (
-          <div key={id} className="table-item">
-            <figure className="figure-item">
-              <img className="table-image" src={thumbnail} alt="music" />
-              <figcaption className="item-description">{group + ' ' + album}</figcaption>
-            </figure>
-            <div>
-              <div className="item-year">{year}</div>
-              <div className="item-format">{format}</div>
+          <Link key={id} to="product">
+            <div key={id} id={String(id)} className="table-item">
+              <figure className="figure-item">
+                <img className="table-image" src={thumbnail} alt="music" />
+                <figcaption className="item-description">{group + ' ' + album}</figcaption>
+              </figure>
+              <div>
+                <div className="item-year">{year}</div>
+                <div className="item-format">{format}</div>
+              </div>
+              <div className="item-price">{price}</div>
+              <button className="item-btn">
+                <Link to="product">DETAILS</Link>
+              </button>
+              <button className="item-btn">
+                <Link to="cart">CART</Link>
+              </button>
             </div>
-            <div className="item-price">{price}</div>
-          </div>
+          </Link>
         );
       })}
     </div>
