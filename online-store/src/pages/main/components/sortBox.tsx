f6d669fa72c9f98cce7d8d605c3b57c87fbb738a
Maxim Chizhov <chizhov98ip@gmail.com>
2022-12-21 13:07:28 +0300

    feat:add checkbox for janres

--- online-store/src/pages/main/components/sortBox.tsx
+++ online-store/src/pages/main/components/sortBox.tsx
@@ -3,36 +3,43 @@ import React, { useState } from 'react';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import { products } from '../../../products';
 
 export default function CheckboxGenre() {
-  const [userinfo, setUserInfo] = useState({
-    genres: [],
-  });
-  const handleChange = (e: { target: { value: unknown; checked: unknown } }) => {
-    // Destructuring
-    const { value, checked } = e.target;
-    const { genres } = userinfo;
+  // const [userinfo, setUserInfo] = useState({
+  // genres: [],
+  // });
+  // const handleChange = (e: { target: { value: unknown; checked: unknown } }) => {
+  // Destructuring
+  // const { value, checked } = e.target;
+  // const { genres } = userinfo;
 
-    console.log(`${value} is ${checked}`);
+  // console.log(`${value} is ${checked}`);
 
-    // Case 1 : The user checks the box
-    if (checked) {
-      setUserInfo({
-        genres: [...genres, value],
-      });
-    }
-    // Case 2  : The user unchecks the box
-    else {
-      setUserInfo({
-        genres: genres.filter((e) => e !== value),
-      });
-    }
-  };
-
+  // Case 1 : The user checks the box
+  // if (checked) {
+  //   setUserInfo({
+  //     genres: [...genres, value],
+  //   });
+  // }
+  // Case 2  : The user unchecks the box
+  // else {
+  // setUserInfo({
+  // genres: genres.filter((e) => e !== value),
+  // });
+  // }
+  // };
+  const uniqCategory = [...new Set(products.map((item) => item.category))];
+  const uniqFormat = [...new Set(products.map((item) => item.format))];
   return (
     <FormGroup>
       <h3>Genre</h3>
-      <FormControlLabel
+      {uniqCategory.map((el, index) => {
+        return (
+          <FormControlLabel key={`key${index}`} control={<Checkbox value={el} />} label={el} />
+        );
+      })}
+      {/* <FormControlLabel
         control={<Checkbox value="Rock" onChange={handleChange} />} 
         label="Rock" />
       <FormControlLabel
@@ -40,7 +47,7 @@ export default function CheckboxGenre() {
         label="Metal"
       />
       <FormControlLabel disabled control={<Checkbox />} label="Punk" />
-      <div>{userinfo.genres}</div>
+      <div>{userinfo.genres}</div> */}
     </FormGroup>
   );
 }
